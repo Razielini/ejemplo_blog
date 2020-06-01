c48a81943f670486713923f8d697a48dbeecb867
Razielini <razielco@gmail.com>
2020-05-31 20:41:36 -0600

    Create usuariosActions.js

---
+++ src/actions/usuariosActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { GET_USUARIOS, CARGANDO, ERROR } from '../types/usuariosTypes'
+
+export const getUsuarios = () => async (dispatch) => {
+  dispatch({
+    type: CARGANDO
+  })
+
+  try {
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
+    dispatch({
+      type: GET_USUARIOS,
+      payload: respuesta.data
+    })
+  } catch (error) {
+    console.log('error ::', error.message)
+    dispatch({
+      type: ERROR,
+      payload: error.message
+    })
+  }
+}
