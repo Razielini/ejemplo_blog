6b93dd8f42d55b9185edb1b5ed356e34c4c6c3ac
Razielini <razielco@gmail.com>
2020-06-01 22:50:34 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Menu from './Menu'
 import usuarios from './usuarios'
+import Publicaciones from './Publicaciones'
+import Tareas from './Tareas'
+import TareasGuardar from './Tareas/Guardar'
 
-
-const Tareas = () => <div className="margen">Tareas</div>
 const App = () => (
   <BrowserRouter>
     <Menu />
     <div className="margen">
       <Route exact path="/" component={ usuarios } />
       <Route exact path="/tareas" component={ Tareas } />
+      <Route exact path="/publicaciones/:key" component={ Publicaciones } />
+      <Route exact path="/tareas/guardar" component={ TareasGuardar } />
+      <Route exact path="/tareas/guardar/:usu_id/:tar_id" component={ TareasGuardar } />
     </div>
   </BrowserRouter>
 )
