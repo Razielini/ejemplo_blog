ba5f56d7b6836567c85ff6aede22badb123057dc
Razielini <razielco@gmail.com>
2020-06-01 22:50:46 -0600

    Update index.js

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,63 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import * as usuariosActions from '../../actions/usuariosActions'
+
 import Spinner from '../general/Spinner'
+import Fatal from '../general/Fatal'
+import Tabla from './Tabla'
+
 /*
   CONST = STATELESS > COMPONENTE FUNCIONA > NO MANEJA ESTADO
   CLASS APP EXTENDS COMPONENT = STATEFUL > COMPONENTE FUNCIONAL
 */
 
 class Usuarios extends Component {
-  filas = () => (
-    this.props.usuarios.map(usuario => (
-      <tr
-        key={usuario.id}
-      >
-        <td>
-          { usuario.name }
-        </td>
-        <td>
-          { usuario.email }
-        </td>
-        <td>
-          { usuario.website }
-        </td>
-      </tr>
-    ))
-  )
-
   componentDidMount () {
-   console.log('componentDidMount ::', this.props.getUsuarios())
-   this.props.getUsuarios()
+   console.log('componentDidMount ::', this.props.usuarios)
+   if (!this.props.usuarios.length) this.props.getUsuarios()
   }
 
   whileLoading = () => {
-    if (this.props.cargando) {
-      return (
-        <Spinner />
-      )
-    }
+    if (this.props.cargando) return <Spinner />
+    if (this.props.error) return <Fatal mensaje={this.props.error} />
 
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          { this.filas() }
-        </tbody>
-      </table>
-    )
+    return <Tabla />
   }
 
   render () {
     console.log('render :: props ::', this.props)
     return (
       <div>
+        <h1>Usuarios</h1>
         { this.whileLoading() }
       </div>
     )
