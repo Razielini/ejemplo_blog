57ef44c26c43a293879b90a2fd99c12d7ac2cb44
Razielini <razielco@gmail.com>
2020-06-01 22:51:10 -0600

    Create publicacionesTypes.js

---
+++ src/types/publicacionesTypes.js
@@ -0,0 +1,7 @@
+// export const GET_PUBLICACIONES = 'traer_publicaciones'
+export const ACTUALIZAR = 'traer_publicaciones_actualizadas'
+export const CARGANDO = 'cargando_publicaciones'
+export const ERROR = 'error_publicaciones'
+export const COM_CARGANDO = 'cargando_publicaciones_comentarios'
+export const COM_ERROR = 'error_publicaciones_comentarios'
+export const COM_ACTUALIZAR = 'comentarios_actualizadar'
